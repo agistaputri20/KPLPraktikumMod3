b02cb087f6bc15bf98027d0cfa1735122729d463
agistaputri20 <agistaputri@student.telkomuniversity.ac.id>
2022-03-12 16:06:27 +0700

    menambahkan class KodeBuah

--- modul3_1302201140/modul3_1302201140/Program.cs
+++ modul3_1302201140/modul3_1302201140/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul3_1302201140
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            KodeBuah.getKodeBuah("Nama Buah", "Kode Buah");
+            KodeBuah.getKodeBuah("Apel", "A00");
+            KodeBuah.getKodeBuah("Aprikot", "B00");
+            KodeBuah.getKodeBuah("Alpukat", "C00");
+            KodeBuah.getKodeBuah("Pisang", "D00");
+            KodeBuah.getKodeBuah("Paprika", "E00");
+            KodeBuah.getKodeBuah("Blackberry", "F00");
+            KodeBuah.getKodeBuah("Ceri", "H00");
+            KodeBuah.getKodeBuah("Kelapa", "I00");
+            KodeBuah.getKodeBuah("Jagung", "J00");
+            KodeBuah.getKodeBuah("Kurma", "K00");
+            KodeBuah.getKodeBuah("Durian", "L00");
+            KodeBuah.getKodeBuah("Anggur", "M00");
+            KodeBuah.getKodeBuah("Melon", "N00");
+            KodeBuah.getKodeBuah("Semangka", "O00");
+        }
+    }
+}
